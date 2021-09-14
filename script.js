e2edf7ea7ad4d4e13fea71165a9b4db114eb6d3f
Ramly23 <88806353+Ramly23@users.noreply.github.com>
2021-09-14 19:32:17 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll('button');
+
+// Let all buttons listen for the click event
+buttons.forEach(button => {
+    button.addEventListener('click', togglePanel)
+})
+
+function togglePanel(event) {
+    let panel = event.target.nextElementSibling
+    
+    // add additional 30px to max-height to accomodate for margins & paddings
+    panel.style.maxHeight ? panel.style.maxHeight = null : panel.style.maxHeight = panel.scrollHeight + 60 + 'px'
+    event.target.classList.toggle('btn--active')
+
+    // make sure all other panels are closed
+    buttons.forEach(one => {
+        if (one != event.target && one.nextElementSibling.style.maxHeight && panel.style.maxHeight){  
+            one.nextElementSibling.style.maxHeight = null
+            one.classList.toggle('btn--active')
+        }
+    })
+}
